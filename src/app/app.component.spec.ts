a891757139820941230d420bf776db5617f84425
jleon253 <jleon253@gmail.com>
2023-11-29 07:33:56 -0500

    Name project changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'app-predict-attack' title`, () => {
+  it(`should have the 'app-police-attack-prediction' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-predict-attack');
+    expect(app.title).toEqual('app-police-attack-prediction');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-predict-attack');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-police-attack-prediction');
   });
 });
